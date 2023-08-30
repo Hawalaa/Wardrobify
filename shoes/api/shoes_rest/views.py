bc7a614bd09412f257b2e98941d1ae72525e1806
Junhao <leongjunho@gmail.com>
2023-08-30 11:51:44 -0700

    Created shoes models and views

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from .models import BinVO, Shoe
+from django.views.decorators.http import require_http_methods
+from common.json import ModelEncoder
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+class BinVODetailEncoder(ModelEncoder):
+    model = BinVO
+    properties = ["closet_name", "bin_number", "bin_size", "import_href"]
+
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["manufacturer", "model_name", "color", "id"]
+
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "model_name",
+        "color",
+        "picture_url",
+        "bin",
+        "id"
+    ]
+    encoders = {
+        "bin": BinVODetailEncoder()
+    }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoes = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        try:
+            bin_href = content["bin"]
+            bin = BinVO.objects.get(import_href=bin_href)
+            content["bin"] = bin
+        except BinVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid bin ID"},
+                status=400,
+            )
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["DELETE"])
+def api_show_shoe(request, pk):
+    if request.method == "DELETE":
+        count, _ = Shoe.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
